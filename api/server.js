406c15ce50914c5f07667e5a4068f32c576da82a
bogbu <glutamine082442@gmail.com>
2024-02-01 19:26:38 +0900

    server 수정

--- api/server.js
+++ api/server.js
@@ -3,7 +3,7 @@ const jsonServer = require('json-server')
 
 const fs = require("fs");
 const os = require("os");
-
+const server = jsonServer.create()
 fs.copyFile("db.json", os.tmpdir() + "/db.json", function (err) {
     if (err) console.log(err);
     else console.log("copy file succeed to" + os.tmpdir());
@@ -19,7 +19,7 @@ fs.copyFile("db.json", os.tmpdir() + "/db.json", function (err) {
 
 // Comment out to allow write operations
 const router = jsonServer.router(path.resolve(os.tmpdir() + "/db.json"));
-const server = jsonServer.create()
+
 const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
